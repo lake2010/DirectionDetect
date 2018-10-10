4c375563b08ec2cfa30c1d571739435d381ad3d9
Administrator <Administrator@MS-20170622GWKD>
2018-10-10 17:46:34 +0800

    添加项目文件。

---
+++ DirectionDetect/DirectionDetectView.cpp
@@ -0,0 +1,96 @@
+
+// DirectionDetectView.cpp : CDirectionDetectView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "DirectionDetect.h"
+#endif
+
+#include "DirectionDetectDoc.h"
+#include "DirectionDetectView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CDirectionDetectView
+
+IMPLEMENT_DYNCREATE(CDirectionDetectView, CView)
+
+BEGIN_MESSAGE_MAP(CDirectionDetectView, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CDirectionDetectView 构造/析构
+
+CDirectionDetectView::CDirectionDetectView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CDirectionDetectView::~CDirectionDetectView()
+{
+}
+
+BOOL CDirectionDetectView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CDirectionDetectView 绘制
+
+void CDirectionDetectView::OnDraw(CDC* /*pDC*/)
+{
+	CDirectionDetectDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+void CDirectionDetectView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CDirectionDetectView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CDirectionDetectView 诊断
+
+#ifdef _DEBUG
+void CDirectionDetectView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CDirectionDetectView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CDirectionDetectDoc* CDirectionDetectView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CDirectionDetectDoc)));
+	return (CDirectionDetectDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CDirectionDetectView 消息处理程序
---
+++ DirectionDetect/DirectionDetectView.h
@@ -0,0 +1,49 @@
+
+// DirectionDetectView.h : CDirectionDetectView 类的接口
+//
+
+#pragma once
+
+
+class CDirectionDetectView : public CView
+{
+protected: // 仅从序列化创建
+	CDirectionDetectView();
+	DECLARE_DYNCREATE(CDirectionDetectView)
+
+// 特性
+public:
+	CDirectionDetectDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CDirectionDetectView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // DirectionDetectView.cpp 中的调试版本
+inline CDirectionDetectDoc* CDirectionDetectView::GetDocument() const
+   { return reinterpret_cast<CDirectionDetectDoc*>(m_pDocument); }
+#endif
+
